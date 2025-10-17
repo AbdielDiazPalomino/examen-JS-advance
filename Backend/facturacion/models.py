01c51a37d7bdd93915c796abe618dfb53f580242
AbdielDiazPalomino <abdoljdp@gmail.com>
2025-10-17 14:48:28 -0500

    Revert "."
    This reverts commit d60b40891707fcf04df21805c9ff59a0abe08ac7.

--- Backend/facturacion/models.py
+++ Backend/facturacion/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Cliente (los compradores)
+# 1Cliente (los compradores)
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     dni = models.CharField(max_length=20, unique=True)
